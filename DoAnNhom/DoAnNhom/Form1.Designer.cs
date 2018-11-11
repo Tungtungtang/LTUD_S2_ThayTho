99479395dfd1044c9249ff4e8ed4d53b36cfe557
nhatanh <33917113+AnhBui1999@users.noreply.github.com>
2018-11-11 22:41:29 +0700

    first commit

---
+++ DoAnNhom/DoAnNhom/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace DoAnNhom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblTK = new System.Windows.Forms.Label();
+            this.lblMK = new System.Windows.Forms.Label();
+            this.txtTK = new System.Windows.Forms.TextBox();
+            this.txtMK = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.SystemColors.Info;
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblTitle.Location = new System.Drawing.Point(26, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(207, 13);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Chao Mung Ban Den Voi Ngan Hang ABC";
+            this.lblTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblTK
+            // 
+            this.lblTK.AutoSize = true;
+            this.lblTK.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblTK.Location = new System.Drawing.Point(13, 46);
+            this.lblTK.Name = "lblTK";
+            this.lblTK.Size = new System.Drawing.Size(29, 13);
+            this.lblTK.TabIndex = 1;
+            this.lblTK.Text = "User";
+            // 
+            // lblMK
+            // 
+            this.lblMK.AutoSize = true;
+            this.lblMK.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblMK.Location = new System.Drawing.Point(13, 87);
+            this.lblMK.Name = "lblMK";
+            this.lblMK.Size = new System.Drawing.Size(56, 13);
+            this.lblMK.TabIndex = 2;
+            this.lblMK.Text = "PassWord";
+            // 
+            // txtTK
+            // 
+            this.txtTK.Location = new System.Drawing.Point(92, 43);
+            this.txtTK.Name = "txtTK";
+            this.txtTK.Size = new System.Drawing.Size(141, 20);
+            this.txtTK.TabIndex = 3;
+            // 
+            // txtMK
+            // 
+            this.txtMK.Location = new System.Drawing.Point(92, 84);
+            this.txtMK.Name = "txtMK";
+            this.txtMK.Size = new System.Drawing.Size(142, 20);
+            this.txtMK.TabIndex = 4;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(165, 239);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(107, 13);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "http://ABCBank.com";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Báo Lỗi";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Đăng Nhập";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.txtMK);
+            this.Controls.Add(this.txtTK);
+            this.Controls.Add(this.lblMK);
+            this.Controls.Add(this.lblTK);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblTK;
+        private System.Windows.Forms.Label lblMK;
+        private System.Windows.Forms.TextBox txtTK;
+        private System.Windows.Forms.TextBox txtMK;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
